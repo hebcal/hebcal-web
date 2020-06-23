8580811067933131303b4a4d5b84d11324334a86
Michael J. Radwin <michael@radwin.org>
2020-06-23 10:42:54 -0700

    Some URL rewriting implemented

--- src/app.js
+++ src/app.js
@@ -1,8 +1,59 @@
 import Koa from 'koa';
+import fs from 'fs';
+import util from 'util';
+import path from 'path';
+import pino from 'pino';
+
+const stat = util.promisify(fs.stat);
+const logger = pino();
+
 const app = new Koa();
 
+// Fix up querystring so we can later use ctx.request.query
+app.use(async (ctx, next) => {
+  if (ctx.request.path.startsWith('/export')) {
+    const semi = ctx.request.querystring.indexOf(';');
+    if (semi != -1) {
+      ctx.request.querystring = ctx.request.querystring.replace(/;/g, '&');
+    }
+  } else if (ctx.request.path.startsWith('/v2')) {
+    const slash = ctx.request.path.indexOf('/', 6);
+    if (slash != -1) {
+      const data = ctx.request.path.substring(6, slash);
+      const filename = ctx.request.path.substring(slash + 1);
+      const buff = Buffer.from(data, 'base64');
+      const qs = buff.toString('ascii');
+      ctx.request.url = '/export/' + filename + '?' + qs;
+    }
+  }
+  return next();
+});
+
+app.use(async (ctx, next) => {
+  console.log(ctx.request.url);
+  if (ctx.request.path == '/robots.txt') {
+    ctx.body = 'User-agent: BadBot\nDisallow: /\n';
+  } else if (ctx.request.path == '/favicon.ico' || ctx.request.path.startsWith('/ical')) {
+    const fpath = path.join(__dirname, ctx.request.path);
+    const fstat = await stat(fpath);
+    if (fstat.isFile()) {
+      ctx.type = path.extname(fpath);
+      ctx.body = fs.createReadStream(fpath);
+    }
+  } else if (ctx.request.path.startsWith('/export')) {
+    console.log(ctx.request.query);
+  }
+  return next();
+});
+
 app.use(async (ctx) => {
-  ctx.body = 'Hello World';
+  logger.info({
+    ip: ctx.request.ip,
+    method: ctx.request.method,
+    url: ctx.request.url,
+    status: ctx.response.status,
+    length: ctx.response.length,
+  });
 });
 
 app.listen(3000);
