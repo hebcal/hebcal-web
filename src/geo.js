db4b77dda2e852073f8f97431f0c7f89177d30bc
Michael J. Radwin <michael@radwin.org>
2020-06-24 10:50:59 -0700

    Hebrew calendar ICS/CSV working

---
+++ src/geo.js
@@ -0,0 +1,105 @@
+import Database from 'better-sqlite3';
+import {Location} from '@hebcal/core';
+import pino from 'pino';
+const city2geonameid = require('./city2geonameid.json');
+
+const logger = pino();
+
+const GEONAME_SQL = `SELECT
+  g.name as name,
+  g.asciiname as asciiname,
+  g.country as cc,
+  c.country as country,
+  a.asciiname as admin1,
+  g.latitude as latitude,
+  g.longitude as longitude,
+  g.timezone as timezone
+FROM geoname g
+LEFT JOIN country c on g.country = c.iso
+LEFT JOIN admin1 a on g.country||'.'||g.admin1 = a.key
+WHERE g.geonameid = ?
+`;
+
+const ZIPCODE_SQL = `SELECT CityMixedCase,State,Latitude,Longitude,TimeZone,DayLightSaving
+FROM ZIPCodes_Primary WHERE ZipCode = ?`;
+
+/** Wrapper around sqlite databases */
+export class GeoDb {
+  /**
+   * @param {string} zipsFilename
+   * @param {string} geonamesFilename
+   */
+  constructor(zipsFilename, geonamesFilename) {
+    this.zipsDb = new Database(zipsFilename, {fileMustExist: true});
+    this.geonamesDb = new Database(geonamesFilename, {fileMustExist: true});
+    this.zipStmt = this.zipsDb.prepare(ZIPCODE_SQL);
+    this.geonamesStmt = this.geonamesDb.prepare(GEONAME_SQL);
+  }
+
+  /** Closes database handles */
+  close() {
+    this.zipsDb.close();
+    this.geonamesDb.close();
+  }
+
+  /**
+   * @param {string} zip
+   * @return {Location}
+   */
+  lookupZip(zip) {
+    const result = this.zipStmt.get(zip);
+    if (!result) {
+      logger.warn(`unknown zipcode=${zip}`);
+      return null;
+    } else if (!result.Latitude && !result.Longitude) {
+      logger.warn(`zero lat/long zipcode=${zip}`);
+      return null;
+    }
+    const tzid = Location.getUsaTzid(result.State, result.TimeZone, result.DayLightSaving);
+    const cityDescr = `${result.CityMixedCase}, ${result.State} ${zip}`;
+    return new Location(result.Latitude, result.Longitude, false, tzid, cityDescr, 'US', zip);
+  }
+
+  /**
+   * @param {number} geonameid
+   * @return {Location}
+   */
+  lookupGeoname(geonameid) {
+    const result = this.geonamesStmt.get(geonameid);
+    if (!result) {
+      logger.warn(`unknown geonameid=${geonameid}`);
+      return null;
+    }
+    const country = result.country || '';
+    const admin1 = result.admin1 || '';
+    const cityDescr = Location.geonameCityDescr(result.asciiname, admin1, country);
+    const location = new Location(
+        result.latitude,
+        result.longitude,
+        result.cc == 'IL',
+        result.timezone,
+        cityDescr,
+        result.cc,
+        geonameid,
+    );
+    if (result.cc == 'IL' && admin1.startsWith('Jerusalem') && result.name.startsWith('Jerualem')) {
+      location.jersualem = true;
+    }
+    return location;
+  }
+
+  /**
+   * @param {string} cityName
+   * @return {Location}
+   */
+  lookupLegacyCity(cityName) {
+    cityName = cityName.replace(/\+/g, ' ');
+    const geonameid = city2geonameid[cityName];
+    if (geonameid) {
+      return this.lookupGeoname(geonameid);
+    } else {
+      logger.warn(`unknown city=${cityName}`);
+      return null;
+    }
+  }
+}
--- src/hebcal.js
+++ src/hebcal.js
@@ -1,9 +1,12 @@
-import {hebcal} from '@hebcal/core';
-import {eventsToIcalendar, eventsToCsv} from '@hebcal/icalendar';
-import Database from 'better-sqlite3';
+import {hebcal, Location} from '@hebcal/core';
+import {eventsToIcalendar, eventsToCsv, getEventCategories} from '@hebcal/icalendar';
+import {GeoDb} from './geo';
+import pino from 'pino';
+
+const logger = pino();
 
 const negativeOpts = {
-  'maj': 'noHolidays',
+  'nx': 'noRoshChodesh',
   'mod': 'noModern',
   'mf': 'noMinorFast',
   'ss': 'noSpecialShabbat',
@@ -14,7 +17,7 @@ const booleanOpts = {
   'D': 'addHebrewDatesForEvents',
   'o': 'omer',
   'a': 'ashkenazi',
-//  'c': 'candlelighting',
+  'c': 'candlelighting',
   'i': 'il',
   's': 'sedrot',
   'F': 'dafyomi',
@@ -22,56 +25,144 @@ const booleanOpts = {
 };
 
 const numberOpts = {
-  'year': 'year',
   'm': 'havdalahMins',
   'b': 'candleLightingMins',
   'ny': 'numYears',
 };
 
 const zipsFilename = 'zips.sqlite3';
-const zipsDb = new Database(zipsFilename, {fileMustExist: true});
-
 const geonamesFilename = 'geonames.sqlite3';
-const geonamesDb = new Database(geonamesFilename, {fileMustExist: true});
+const db = new GeoDb(zipsFilename, geonamesFilename);
+
+/**
+ * @param {string} val
+ * @return {boolean}
+ */
+function empty(val) {
+  return typeof val == 'undefined' || !val.length;
+}
+
+const geoposLegacy = {
+  ladeg: 90,
+  lamin: 60,
+  lodeg: 180,
+  lomin: 60,
+};
+
+const lgToLocale = {
+  h: 'he',
+  a: 'ashkenazi',
+  ah: 'ashkenazi',
+  s: 's',
+  sh: 's',
+};
 
 /**
  * @param {Koa.ParameterizedContext<Koa.DefaultState, Koa.DefaultContext>} ctx
  */
 export async function hebcalDownload(ctx) {
-  if (ctx.request.query.v !== '1') {
+  const query = ctx.request.query;
+  if (query.v !== '1') {
     return;
   }
   const options = {};
   for (const [key, val] of Object.entries(booleanOpts)) {
-    if (typeof ctx.request.query[key] == 'string' &&
-      (ctx.request.query[key] == 'on' || ctx.request.query[key] == '1')) {
+    if (typeof query[key] == 'string' &&
+      (query[key] == 'on' || query[key] == '1')) {
       options[val] = true;
     }
   }
   for (const [key, val] of Object.entries(negativeOpts)) {
-    if (typeof ctx.request.query[key] == 'undefined' ||
-        ctx.request.query[key] == 'off' || ctx.request.query[key] == '0') {
-      options[val] = false;
+    if (typeof query[key] == 'undefined' ||
+        query[key] == 'off' || query[key] == '0') {
+      options[val] = true;
     }
+  }
+  if (typeof query.nh == 'undefined' && typeof query.maj == 'undefined') {
+    options.noHolidays = true;
+  }
+  // legacy: before we had maj/min/mod/ss/mf, we only had nh/nx.
+  // disable minor holidays only if we are sure it's not an old URL
+  if (query.nh != 'on' || query.nx != 'on') {
+    options.noMinorHolidays = (typeof query.min == 'undefined');
   }
   for (const [key, val] of Object.entries(numberOpts)) {
-    if (typeof ctx.request.query[key] == 'string' && ctx.request.query[key].length) {
-      options[val] = +ctx.request.query[key];
+    if (typeof query[key] == 'string' && query[key].length) {
+      options[val] = +query[key];
     }
   }
-  if (ctx.request.query.yt) {
-    options.isHebrewDate = Boolean(ctx.request.query.yt == 'H');
+  if (!empty(query.year) && query.year != 'now') {
+    options.year = +query.year;
   }
-  if (ctx.request.query.month) {
-    const month = +ctx.request.query.month;
+  if (!empty(query.yt)) {
+    options.isHebrewDate = Boolean(query.yt == 'H');
+  }
+  if (!empty(query.month)) {
+    const month = +query.month;
     if (month >= 1 && month <= 12) {
       options.month = month;
     }
   }
-  if (ctx.request.query.lg) {
-    options.locale = ctx.request.query.lg;
+  if (!empty(query.lg)) {
+    options.locale = lgToLocale[query.lg] || query.lg;
   }
-  const events = hebcal.hebrewCalendar(options);
+  let location;
+  if (!empty(query.zip)) {
+    location = db.lookupZip(query.zip);
+  } else if (!empty(query.city)) {
+    location = db.lookupLegacyCity(query.city);
+  } else if (!empty(query.geonameid)) {
+    location = db.lookupGeoname(+query.geonameid);
+  } else if (query.geo == 'pos') {
+    if (!empty(query.latitude) && !empty(query.longitude)) {
+      if (empty(query.tzid)) {
+        throw new Error('Timezone required');
+      }
+      if (query.tzid == 'Asia/Jerusalem') {
+        options.il = true;
+      }
+      location = new Location(+query.latitude, +query.longitude, options.il, query.tzid);
+    } else {
+      let tzid = query.tzid;
+      if (empty(tzid) && !empty(query.tz) && !empty(query.dst)) {
+        tzid = Location.legacyTzToTzid(query.tz, query.dst);
+      }
+      if (!tzid) {
+        throw new Error('Timezone required');
+      }
+      for (const [key, max] of Object.entries(geoposLegacy)) {
+        if (empty(query[key]) || +query[key] > max) {
+          throw new RangeError(`Sorry, ${key}=${query[key]} out of valid range 0-${max}`);
+        }
+      }
+      let latitude = +query.ladeg + (+query.lamin / 60.0);
+      let longitude = +query.lodeg + (+query.lomin / 60.0);
+      if (query.ladir == 's') {
+        latitude *= -1;
+      }
+      if (query.lodir == 'w') {
+        longitude *= -1;
+      }
+      if (tzid == 'Asia/Jerusalem') {
+        options.il = true;
+      }
+      location = new Location(latitude, longitude, options.il, tzid);
+    }
+  }
+  if (location) {
+    options.location = location;
+  }
+  logger.info(options);
+  let events = hebcal.hebrewCalendar(options);
+  if (options.noMinorHolidays) {
+    events = events.filter((ev) => {
+      const categories = getEventCategories(ev);
+      if (categories.length > 1 && categories[1] == 'minor') {
+        return false;
+      }
+      return true;
+    });
+  }
   if (ctx.request.path.endsWith('.ics')) {
     const ical = eventsToIcalendar(events, options);
     ctx.response.type = 'text/calendar; charset=utf-8';
