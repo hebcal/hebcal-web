25ca663c1b1468aaa9348b4cfa8834d0f9c45882
Michael J. Radwin <michael@radwin.org>
2020-06-26 16:46:05 -0700

    Handle missing yahrzeit args more gracefully

--- src/yahrzeit.js
+++ src/yahrzeit.js
@@ -6,47 +6,21 @@ import dayjs from 'dayjs';
  * @param {Koa.ParameterizedContext<Koa.DefaultState, Koa.DefaultContext>} ctx
  */
 export async function yahrzeitDownload(ctx) {
-  if (ctx.request.query.v !== 'yahrzeit') {
+  const query = ctx.request.query;
+  if (query.v !== 'yahrzeit') {
     return;
   }
-  const ids = Object.keys(ctx.request.query)
+  const ids = Object.keys(query)
       .filter((k) => k[0] == 't').map((k) => +(k.substring(1)));
-  const max = Math.max(...ids);
-  const years = +ctx.request.query.years || 20;
+  const maxId = Math.max(...ids);
+  const years = +query.years || 20;
   const startYear = new HDate().getFullYear();
   const endYear = startYear + years;
-  const yizkor = Boolean(ctx.request.query.yizkor && ctx.request.query.yizkor == 'on');
-  const hebdate = Boolean(ctx.request.query.hebdate && ctx.request.query.hebdate == 'on');
   let events = [];
-  for (let id = 1; id <= max; id++) {
-    const [type, dd, mm, yy, sunset, name] = [
-      ctx.request.query[`t${id}`],
-      ctx.request.query[`d${id}`],
-      ctx.request.query[`m${id}`],
-      ctx.request.query[`y${id}`],
-      ctx.request.query[`s${id}`],
-      ctx.request.query[`n${id}`],
-    ];
-    let day = dayjs(new Date(yy, mm - 1, dd));
-    if (sunset == 'on') {
-      day = day.add(1, 'day');
-    }
-    for (let hyear = startYear; hyear <= endYear; hyear++) {
-      const hd = (type == 'Yahrzeit') ?
-        hebcal.getYahrzeit(hyear, day.toDate()) :
-        hebcal.getBirthdayOrAnniversary(hyear, day.toDate());
-      if (hd) {
-        const typeStr = (type == 'Yahrzeit') ? type : `Hebrew ${type}`;
-        let subj = `${name}'s ${typeStr}`;
-        if (hebdate) {
-          subj += ' (' + new HebrewDateEvent(hd).render() + ')';
-        }
-        const ev = new Event(hd, subj, flags.USER_EVENT);
-        events.push(ev);
-      }
-    }
+  for (let id = 1; id <= maxId; id++) {
+    events = events.concat(getEventsForId(query, id, startYear, endYear));
   }
-  if (yizkor) {
+  if (query.yizkor == 'on') {
     const holidays = makeYizkorEvents(startYear, endYear);
     events = events.concat(holidays);
   }
@@ -56,11 +30,51 @@ export async function yahrzeitDownload(ctx) {
     ctx.response.type = 'text/calendar; charset=utf-8';
     ctx.body = ical;
   } else if (ctx.request.path.endsWith('.csv')) {
-    const euro = Boolean(ctx.request.query.euro);
+    const euro = Boolean(query.euro);
     const ical = eventsToCsv(events, {euro});
     ctx.response.type = 'text/x-csv; charset=utf-8';
     ctx.body = ical;
   }
+}
+
+/**
+ * @param {any} query
+ * @param {number} id
+ * @param {number} startYear
+ * @param {number} endYear
+ * @return {Event[]}
+ */
+function getEventsForId(query, id, startYear, endYear) {
+  const events = [];
+  const [type, dd, mm, yy] = [
+    query[`t${id}`],
+    query[`d${id}`],
+    query[`m${id}`],
+    query[`y${id}`],
+  ];
+  if (!type || !dd || !mm || !yy) {
+    return events;
+  }
+  const sunset = query[`s${id}`];
+  const name = query[`n${id}`] || `Person${id}`;
+  let day = dayjs(new Date(yy, mm - 1, dd));
+  if (sunset == 'on') {
+    day = day.add(1, 'day');
+  }
+  for (let hyear = startYear; hyear <= endYear; hyear++) {
+    const hd = (type == 'Yahrzeit') ?
+        hebcal.getYahrzeit(hyear, day.toDate()) :
+        hebcal.getBirthdayOrAnniversary(hyear, day.toDate());
+    if (hd) {
+      const typeStr = (type == 'Yahrzeit') ? type : `Hebrew ${type}`;
+      let subj = `${name}'s ${typeStr}`;
+      if (query.hebdate == 'on') {
+        subj += ' (' + hd.render('en') + ')';
+      }
+      events.push(new Event(hd, subj, flags.USER_EVENT));
+    }
+  }
+  return events;
 }
 
 /**
