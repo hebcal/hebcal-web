1408f40f365bad003e931543c3a9a97b9fbc781d
Michael J. Radwin <michael@radwin.org>
2020-07-19 11:35:21 -0700

    Move Shabbat autocomplete scripts from ejs to js to prevent leakage across requests

--- src/shabbat.js
+++ src/shabbat.js
@@ -151,6 +151,17 @@ function makePropsForFullHtml(ctx) {
     summary: briefText.join('. '),
     jsonLD: firstCandles ? JSON.stringify(getJsonLD(firstCandles, location)) : '',
     locationName: location.getName(),
+    xtra_html:
+`<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.10.4/typeahead.bundle.min.js"></script>
+<script src="https://www.hebcal.com/i/hebcal-app-1.9.min.js"></script>
+<script>
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'));
+var tooltipList = tooltipTriggerList.map(function (el) {
+  return new bootstrap.Tooltip(el);
+});
+window['hebcal'].createCityTypeahead(false);
+</script>`,
   };
 }
 
