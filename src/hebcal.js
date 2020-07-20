858fee6aafc00c70f6027feffd428a9a48796951
Michael J. Radwin <michael@radwin.org>
2020-07-20 12:55:09 -0700

    Hebcal form processing and json/e/e2/fc output

--- src/hebcal.js
+++ src/hebcal.js
@@ -1,6 +1,8 @@
 /* eslint-disable require-jsdoc */
-import {getLocationFromQuery, processCookieAndQuery} from './common';
-import {HDate} from '@hebcal/core';
+import {makeHebcalOptions, processCookieAndQuery} from './common';
+import {HebrewCalendar} from '@hebcal/core';
+import {eventsToClassicApi, eventToFullCalendar} from '@hebcal/rest-api';
+import dayjs from 'dayjs';
 
 const hebcalFormDefaults = {
   maj: 'on',
@@ -27,33 +29,91 @@ export async function hebcalApp(ctx) {
   const q = (ctx.request.querystring.length === 0 && !cookie) ? hebcalFormDefaults :
     ctx.request.query.v === '1' ? ctx.request.query :
     processCookieAndQuery(cookie, hebcalFormDefaults, ctx.request.query);
-  if (q.year === 'now') {
-    q.year = q.yt === 'H' ? new HDate().getFullYear() : new Date().getFullYear();
-  }
-  let location;
+  let error;
+  let options;
   try {
-    location = getLocationFromQuery(ctx.db, q, q.i === 'on');
-    if (location) {
-      q['city-typeahead'] = location.getName();
-      if (location.getIsrael()) {
-        q.i = 'on';
+    options = makeHebcalOptions(ctx.db, q);
+  } catch (err) {
+    error = err;
+  }
+  if (options.il) {
+    q.i = 'on';
+  }
+  if (options.location) {
+    q['city-typeahead'] = options.location.getName();
+  }
+  if (q.year === 'now') {
+    q.year = options.year;
+  }
+  if (q.cfg === 'json') {
+    ctx.set('Cache-Control', 'max-age=86400');
+    const events = HebrewCalendar.calendar(options);
+    // console.log(options);
+    // console.log(events);
+    let obj = eventsToClassicApi(events, options);
+    if (q.leyning === 'off') {
+      for (const item of obj.items) {
+        delete item.leyning;
       }
     }
-  } catch (err) {
-  }
-  await ctx.render('hebcal-form', {
-    q,
-    location,
-    title: 'Custom Calendar | Hebcal Jewish Calendar',
-    xtra_html: `<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.10.4/typeahead.bundle.min.js"></script>
-<script src="https://www.hebcal.com/i/hebcal-app-1.9.min.js"></script>
-<script>
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'));
-var tooltipList = tooltipTriggerList.map(function (el) {
-return new bootstrap.Tooltip(el);
+    const cb = q.callback;
+    if (typeof cb === 'string' && cb.length) {
+      obj = cb + '(' + JSON.stringify(obj) + ')\n';
+    }
+    ctx.body = obj;
+  } else if (q.cfg === 'fc') {
+    options.start = new Date(q.start);
+    options.end = new Date(q.end);
+    const events = HebrewCalendar.calendar(options);
+    const location = options.location;
+    const tzid = location ? location.getTzid() : 'UTC';
+    ctx.body = events.map((ev) => eventToFullCalendar(ev, tzid, options.il));
+  } else if (q.cfg === 'e' || q.cfg === 'e2') {
+    options.numYears = 2;
+    const events = HebrewCalendar.calendar(options);
+    if (q.cfg === 'e') {
+      const strs = events.map((ev) => {
+        const d = dayjs(ev.getDate().greg());
+        const fmtDt = d.format('YYYYMMDD');
+        const desc = ev.render();
+        const url = ev.url() || '';
+        return `DefineEvent(${fmtDt},"${desc}","${url}","",0,0);`;
+      });
+      ctx.body = strs.join('\n');
+    } else {
+      const strs = events.map((ev) => {
+        const d = dayjs(ev.getDate().greg());
+        const url = ev.url();
+        const obj = {d: d.format('YYYYMMDD'), s: ev.render()};
+        if (url) {
+          obj.u = url;
+        }
+        return JSON.stringify(obj);
+      });
+      ctx.body = 'if(typeof HEBCAL=="undefined"||!HEBCAL){var HEBCAL={};}\nHEBCAL.eraw=[\n' +
+        strs.join(',') + `];
+HEBCAL.jec2events=HEBCAL.eraw.map(function(e){
+  var f={eventDate:e.d,eventDescription:e.s};
+  if(e.u){f.eventLink=e.u}
+  return f;
 });
-window['hebcal'].createCityTypeahead(false);
-</script>`,
+`;
+    }
+  } else {
+    return ctx.render('hebcal-form', {
+      q,
+      location: options.location,
+      title: 'Custom Calendar | Hebcal Jewish Calendar',
+      xtra_html: `<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.10.4/typeahead.bundle.min.js"></script>
+  <script src="https://www.hebcal.com/i/hebcal-app-1.9.min.js"></script>
+  <script>
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'));
+  var tooltipList = tooltipTriggerList.map(function (el) {
+  return new bootstrap.Tooltip(el);
   });
+  window['hebcal'].createCityTypeahead(false);
+  </script>`,
+    });
+  }
 }
