abdbf921160eb46dde9ea2232846f3e582d8ded8
Michael J. Radwin <michael@radwin.org>
2020-08-09 17:03:27 -0700

    Progress on email subscription

--- src/email.js
+++ src/email.js
@@ -37,7 +37,7 @@ FROM hebcal_shabbat_email
 WHERE hebcal_shabbat_email.email_id = ?`;
   const results = await db.query(sql, subscriptionId);
   if (!results || !results[0]) {
-    db.close();
+    await db.close();
     ctx.throw(404, `Subscription confirmation key ${subscriptionId} not found`);
   }
   const row = results[0];
@@ -47,7 +47,7 @@ WHERE hebcal_shabbat_email.email_id = ?`;
   if (confirmed) {
     await updateDbAndEmail(ctx, db);
   }
-  db.close();
+  await db.close();
   const title = confirmed ? 'Email Subscription Confirmed' : 'Confirm Email Subscription';
   await ctx.render('verify', {
     title: `${title} | Hebcal Jewish Calendar`,
@@ -131,6 +131,18 @@ export async function emailForm(ctx) {
       {},
       Object.assign({}, ctx.request.body || {}, ctx.request.query),
   );
+  let defaultUnsubscribe = false;
+  if (typeof q.e === 'string') {
+    const buff = Buffer.from(q.e, 'base64');
+    q.em = buff.toString('ascii');
+    const db = makeDb(ctx.iniConfig);
+    const subInfo = await getSubInfo(db, q.em);
+    if (subInfo && subInfo.status === 'active') {
+      Object.assign(q, subInfo);
+    }
+    await db.close();
+    defaultUnsubscribe = q.unsubscribe === '1';
+  }
   let location;
   try {
     location = getLocationFromQuery(ctx.db, q);
@@ -138,9 +150,83 @@ export async function emailForm(ctx) {
     ctx.state.message = err.message;
   }
   q['city-typeahead'] = location ? location.getName() : '';
+  ctx.state.title = 'Shabbat Candle Lighting Times by Email | Hebcal Jewish Calendar';
+  if (q.v === '1') {
+    if (!q.em) {
+      ctx.state.message = 'Please enter your email address.';
+    } else if (!validateEmail(q.em)) {
+      ctx.state.message = `Invalid email address ${q.em}`;
+    } else if (q.unsubscribe === '1') {
+      const ok = await unsubscribe(ctx, q);
+      if (ok) {
+        await ctx.render('email-unsubscribe');
+        return Promise.resolve(true);
+      }
+    } else if (q.modify === '1' && !location) {
+      ctx.state.message = 'Please enter your location.';
+    } else if (q.modify === '1' && !ctx.state.message) {
+      await ctx.render('email-success', {
+        emailAddress: q.em,
+        locationName: location.getName(),
+      });
+      return Promise.resolve(true);
+    }
+  }
   await ctx.render('email', {
-    title: `Shabbat Candle Lighting Times by Email | Hebcal Jewish Calendar`,
     q,
     xtra_html: tooltipScript + typeaheadScript,
+    defaultUnsubscribe,
   });
+}
+
+function validateEmail(email) {
+  // eslint-disable-next-line max-len
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+async function unsubscribe(ctx, q) {
+  const db = makeDb(ctx.iniConfig);
+  const subInfo = await getSubInfo(db, q.em);
+  ctx.state.emailAddress = q.em;
+  let success;
+  if (!subInfo) {
+    ctx.state.message = `Sorry, ${q.em} is not currently subscribed`;
+    success = false;
+  } else if (subInfo.status === 'unsubscribed') {
+    ctx.state.success = false;
+    success = true;
+  } else {
+    const sqlUpdate = `UPDATE hebcal_shabbat_email
+    SET email_status = 'unsubscribed', email_ip = ?
+    WHERE email_address = ?`;
+    const ip = ctx.request.headers['x-client-ip'] || ctx.request.ip;
+    await db.query(sqlUpdate, [ip, q.em]);
+    ctx.state.success = true;
+    success = true;
+  }
+  await db.close();
+  return success;
+}
+
+async function getSubInfo(db, emailAddress) {
+  const sql = `
+  SELECT email_id, email_address, email_status, email_created,
+    email_candles_zipcode, email_candles_city,
+    email_candles_geonameid,
+    email_candles_havdalah, email_optin_announce
+  FROM hebcal_shabbat_email
+  WHERE hebcal_shabbat_email.email_address = ?`;
+  const results = await db.query(sql, emailAddress);
+  if (!results || !results[0]) {
+    return null;
+  }
+  const r = results[0];
+  return Object.assign({
+    k: r.email_id,
+    em: r.email_address,
+    status: r.email_status,
+    m: String(r.email_candles_havdalah),
+    t: r.email_created,
+  }, getGeoFromRow(r));
 }
