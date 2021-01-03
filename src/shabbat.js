c34194920a79efda8b0b559e2b348b753e24cf8f
Michael J. Radwin <michael@radwin.org>
2021-01-03 09:51:55 -0800

    hack for client who passes +03:00

--- src/shabbat.js
+++ src/shabbat.js
@@ -107,6 +107,14 @@ function makeItems(ctx) {
       ctx.request.query,
   );
   q.c = q.s = 'on';
+  // hack for client who passes +03:00
+  if (q.tzid && q.tzid[0] === ' ') {
+    console.log(q.tzid);
+    const m = q.tzid.match(/^ (\d\d):00$/);
+    if (m && m[1]) {
+      q.tzid = 'Etc/GMT+' + parseInt(m[1], 10);
+    }
+  }
   let opts0 = {};
   try {
     opts0 = makeHebcalOptions(ctx.db, q);
