5be523327fcbb116bb8df8322e67781e1811b8f6
Michael J. Radwin <michael@radwin.org>
2021-01-27 10:19:50 -0800

    Support both single zmanim date and batch (up to 45 day range) queries

--- src/zmanim.js
+++ src/zmanim.js
@@ -5,10 +5,11 @@ import createError from 'http-errors';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
-
+dayjs.extend(isSameOrBefore);
 
 const TIMES = {
   alotHaShachar: 1,
@@ -28,6 +29,8 @@ const TIMES = {
   chatzotNight: 1,
 };
 
+const MAX_DAYS = 45;
+
 /**
  * @private
  * @param {any} ctx
@@ -35,39 +38,97 @@ const TIMES = {
 export async function getZmanim(ctx) {
   ctx.response.type = ctx.request.header['accept'] = 'application/json';
   const q = ctx.request.query;
-  const dateStr = q.date;
-  const isToday = empty(dateStr);
-  const dt = isToday ? new Date() : isoDateStringToDate(dateStr);
-  const isoDate = dt.toISOString().substring(0, 10);
+  const {isRange, startD, endD} = getStartAndEnd(q);
   const loc = ctx.state.location = getLocationFromQuery(ctx.db, q);
   if (loc === null) {
     throw createError(400, 'Location is required');
   }
-  if (isToday) {
-    expires(ctx);
-  } else {
+  if (isRange || !empty(q.date)) {
     ctx.set('Cache-Control', 'max-age=2592000');
+  } else {
+    expires(ctx);
   }
-  const zman = new Zmanim(dt, loc.getLatitude(), loc.getLongitude());
-  const times = {};
-  const tzid = loc.getTzid();
   const timeFormat = new Intl.DateTimeFormat('en-US', {
-    timeZone: tzid,
+    timeZone: loc.getTzid(),
     hour12: false,
     hour: 'numeric',
     minute: 'numeric',
   });
-  for (const func of Object.keys(TIMES)) {
-    times[func] = {};
+  if (isRange) {
+    const times = {};
+    for (const func of Object.keys(TIMES)) {
+      times[func] = {};
+    }
+    times.tzeit7083 = {};
+    for (let d = startD; d.isSameOrBefore(endD, 'd'); d = d.add(1, 'd')) {
+      const dt = d.toDate();
+      const t = getTimes(dt, loc, timeFormat);
+      const isoDate = dt.toISOString().substring(0, 10);
+      for (const [key, val] of Object.entries(t)) {
+        times[key][isoDate] = val;
+      }
+    }
+    ctx.body = {location: loc, times};
+  } else {
+    const dt = startD.toDate();
+    const t = getTimes(dt, loc, timeFormat);
+    const isoDate = dt.toISOString().substring(0, 10);
+    ctx.body = {location: loc, date: isoDate, times: t};
   }
+}
+
+// eslint-disable-next-line require-jsdoc
+function getStartAndEnd(q) {
+  if (!empty(q.start) && empty(q.end)) {
+    q.end = q.start;
+  } else if (empty(q.start) && !empty(q.end)) {
+    q.start = q.end;
+  }
+  if (!empty(q.start) && !empty(q.end) && q.start === q.end) {
+    q.date = q.start;
+    delete q.start;
+    delete q.end;
+  }
+  let isRange = !empty(q.start) && !empty(q.end);
+  const startD = isRange ? isoToDayjs(q.start) : empty(q.date) ? dayjs() : isoToDayjs(q.date);
+  let endD = isRange ? isoToDayjs(q.end) : empty(q.date) ? dayjs() : isoToDayjs(q.date);
+  if (isRange) {
+    if (endD.isBefore(startD, 'd')) {
+      isRange = false;
+      endD = startD;
+    } else if (endD.diff(startD, 'd') > MAX_DAYS) {
+      endD = startD.add(MAX_DAYS, 'd');
+    }
+  }
+  return {isRange, startD, endD};
+}
+
+/**
+ * @param {string} str
+ * @return {dayjs.Dayjs}
+ */
+function isoToDayjs(str) {
+  return dayjs(isoDateStringToDate(str));
+}
+
+/**
+ * @private
+ * @param {Date} dt
+ * @param {Location} location
+ * @param {Intl.DateTimeFormat} timeFormat
+ * @return {Object<string,string>}
+ */
+function getTimes(dt, location, timeFormat) {
+  const tzid = location.getTzid();
+  const times = {};
+  const zman = new Zmanim(dt, location.getLatitude(), location.getLongitude());
   for (const func of Object.keys(TIMES)) {
     const val = zman[func]();
-    times[func][isoDate] = toISOStringWithTimezone(val, formatTime(timeFormat, val), tzid);
+    times[func] = toISOStringWithTimezone(val, formatTime(timeFormat, val), tzid);
   }
   const tzeit7 = zman.tzeit(7.083);
-  times['tzeit7083'] = {};
-  times['tzeit7083'][isoDate] = toISOStringWithTimezone(tzeit7, formatTime(timeFormat, tzeit7), tzid);
-  ctx.body = {location: loc, times};
+  times['tzeit7083'] = toISOStringWithTimezone(tzeit7, formatTime(timeFormat, tzeit7), tzid);
+  return times;
 }
 
 /**
