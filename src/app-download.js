0874b943a4d422e15ab3ab0548f85027961d87e4
Michael J. Radwin <michael@radwin.org>
2021-02-15 19:55:28 -0800

    Flag yahrzeit legacy redirects with redir=1

--- src/app-download.js
+++ src/app-download.js
@@ -98,14 +98,14 @@ app.use(async (ctx, next) => {
     // note we use unescape() instead of decodeURIComponent() due to ancient latin-1 encoding
     if (ctx.request.querystring.startsWith('subscribe=1%3B') || ctx.request.querystring.startsWith('dl=1%3B')) {
       const qs = unescape(ctx.request.querystring).replace(/;/g, '&');
-      httpRedirect(ctx, `${path}?${qs}`, 301);
+      httpRedirect(ctx, `${path}?redir=1&${qs}`, 301);
       return;
     } else {
       const encQuery = path.indexOf('.ics%3Fsubscribe%3D1');
       if (encQuery != -1) {
         const qs = unescape(path.substring(encQuery + 7)).replace(/;/g, '&');
         const path2 = path.substring(0, encQuery + 4);
-        httpRedirect(ctx, `${path2}?${qs}`, 301);
+        httpRedirect(ctx, `${path2}?redir=1&${qs}`, 301);
         return;
       }
     }
