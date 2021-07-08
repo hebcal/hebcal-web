83792eb94bae843fdb42a5619ae09262a82542d7
Michael J. Radwin <michael@radwin.org>
2021-07-08 09:49:30 -0700

    Allow triennial in far-future years except for the 8 cycles where Vayakhel-Pekudei is read separately each year

--- src/sedrot.js
+++ src/sedrot.js
@@ -79,6 +79,18 @@ function get15yrEvents(parshaName, il) {
 
 const parshaDateRe = /^([a-z-]+)-(\d{8})$/;
 
+// Can't find pattern SSS for Vayakhel-Pekudei, startYear=5831
+const noTriennial = [
+  5831, 5832, 5833,
+  6572, 6573, 6574,
+  7313, 7314, 7315,
+  7340, 7341, 7342,
+  8081, 8082, 8083,
+  8822, 8823, 8824,
+  9194, 9195, 9196,
+  9935, 9936, 9937,
+];
+
 export async function parshaDetail(ctx) {
   const rpath = ctx.request.path;
   const base = basename(rpath);
@@ -132,7 +144,7 @@ export async function parshaDetail(ctx) {
   parsha.haftaraHref = getHaftarahHref(parsha.haftara);
   const hd = parshaEv.getDate();
   const hyear = hd.getFullYear();
-  const hasTriennial = !il && hyear >= 5745 && hyear <= 5830;
+  const hasTriennial = !il && hyear >= 5744 && noTriennial.indexOf(hyear) === -1;
   const triennial = hasTriennial ? makeTriennial(date, parshaEv, hyear, parshaName) : {};
   const titleYear = date ? ' ' + hyear : '';
   const titleHebrew = Locale.hebrewStripNikkud(parsha.hebrew);
