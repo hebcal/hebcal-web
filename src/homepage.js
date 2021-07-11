e16fbc7df9b742080173ccfe3bb19df77aaa9c9a
Michael J. Radwin <michael@radwin.org>
2021-07-10 22:00:28 -0700

    swap moon and calendar emoji for Rosh Chodesh

--- src/homepage.js
+++ src/homepage.js
@@ -248,6 +248,6 @@ Pesach begins at sundown on ${strtime}`];
 function getRoshChodeshGreeting(ev) {
   const monthName = ev.getDesc().substring(13); // 'Rosh Chodesh '
   const url = ev.url();
-  return ['🌒🗓️&nbsp; Chodesh Tov / <span lang="he" dir="rtl">חודש טוב</span> &nbsp;🗓️🌒',
+  return ['🗓️ 🌒&nbsp; Chodesh Tov / <span lang="he" dir="rtl">חודש טוב</span> &nbsp;🌒 🗓️',
     `<br>We wish you a good new month of <a href="${url}">${monthName}</a>`];
 }
