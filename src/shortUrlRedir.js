09ef8357f663c9f94ea6078e38bccd07eb494221
Michael J. Radwin <michael@radwin.org>
2021-08-12 14:44:49 -0700

    Allow short utm parameters

--- src/shortUrlRedir.js
+++ src/shortUrlRedir.js
@@ -4,16 +4,26 @@ import querystring from 'querystring';
 // eslint-disable-next-line require-jsdoc
 export function shortUrlRedir(ctx) {
   const qs = querystring.parse(ctx.request.querystring || '');
-  const ref = ctx.get('referrer');
+  if (qs.us) {
+    qs.utm_source = qs.us;
+    delete qs.us;
+  }
+  if (qs.um) {
+    qs.utm_medium = qs.um;
+    delete qs.um;
+  }
+  if (qs.uc) {
+    qs.utm_campaign = qs.uc;
+    delete qs.uc;
+  }
+  const ref = ctx.get('referer');
   if (!qs.utm_source && ref && ref.length) {
     const refUrl = new URL(ref);
     if (!refUrl.hostname.endsWith('hebcal.com')) {
       qs.utm_source = refUrl.hostname;
     }
   }
-  if (!qs.utm_source) {
-    qs.utm_source = 'redir';
-  }
+  qs.utm_source = qs.utm_source || 'redir';
   qs.utm_medium = qs.utm_medium || 'redir';
   const rpath = ctx.request.path;
   const base = path.basename(rpath);
