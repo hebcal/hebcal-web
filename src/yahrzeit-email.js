bc4ed130ecab54078b59a3f8e42090faa88701f8
Michael J. Radwin <michael@radwin.org>
2022-02-09 15:42:19 -0800

    Use INSERT, not REPLACE, since we dropped the unique key

--- src/yahrzeit-email.js
+++ src/yahrzeit-email.js
@@ -92,7 +92,7 @@ export async function yahrzeitEmailSub(ctx) {
       const ip = getIpAddress(ctx);
       await db.query(sqlUpdate, [ip, q.id]);
     }
-    const sql = 'REPLACE INTO yahrzeit_optout (email_id, name_hash, num, deactivated) VALUES (?, ?, ?, 1)';
+    const sql = 'INSERT INTO yahrzeit_optout (email_id, name_hash, num, deactivated) VALUES (?, ?, ?, 1)';
     const num = q.num === 'all' ? 0 : parseInt(q.num, 10);
     const nameHash = num == 0 ? null : (q.hash || null);
     await db.query(sql, [q.id, nameHash, num]);
