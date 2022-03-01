61d0e6be5e6b6fcb72d9b23b3dac4e41e7d96e78
Michael J. Radwin <michael@radwin.org>
2022-03-01 10:44:23 -0800

    Reduce analytics noise by skipping tracking pv from large well-known robots

--- src/analytics.js
+++ src/analytics.js
@@ -5,6 +5,20 @@ import util from 'util';
 // return array that have 4 elements of 32bit integer
 const murmur128 = util.promisify(mmh3.murmur128);
 
+const knownRobots = {
+  'Mediapartners-Google': 1,
+  'Mozilla/5.0 (compatible; AhrefsBot/7.0; +http://ahrefs.com/robot/)': 1,
+  'Mozilla/5.0 (compatible; BLEXBot/1.0; +http://webmeup-crawler.com/)': 1,
+  'Mozilla/5.0 (compatible; DotBot/1.2; +https://opensiteexplorer.org/dotbot; help@moz.com)': 1,
+  'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)': 1,
+  'Mozilla/5.0 (compatible; SemrushBot/7~bl; +http://www.semrush.com/bot.html)': 1,
+  'Mozilla/5.0 (compatible; YandexBot/3.0; +http://yandex.com/bots)': 1,
+  'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)': 1,
+  'Rainmeter WebParser plugin': 1,
+  'Varnish Health Probe': 1,
+  'check_http/v2.2 (monitoring-plugins 2.2)': 1,
+};
+
 /**
  * Middleware to track via Google Analytics only if
  * `ctx.state.trackPageview === true && ctx.status === 200`
@@ -28,8 +42,12 @@ export function googleAnalytics(tid) {
       visitorId = await makeUuid(ipAddress, userAgent, ctx.get('accept-language'));
     }
     ctx.state.visitorId = visitorId;
-    const options = {tid: ctx.state.trackingId || tid, cid: visitorId, enableBatching: true};
-    const visitor = ctx.state.visitor = ua(options);
+    const options = {enableBatching: true};
+    if (cookie.has('uid')) {
+      options.uid = cookie.get('uid');
+    }
+    tid = ctx.state.trackingId || tid;
+    const visitor = ctx.state.visitor = ua(tid, visitorId, options);
     visitor.set('ua', userAgent);
     visitor.set('dr', ctx.get('referrer'));
     visitor.set('uip', ipAddress);
@@ -37,7 +55,11 @@ export function googleAnalytics(tid) {
       visitor.set('uid', cookie.get('uid'));
     }
     await next();
-    if (ctx.state.trackPageview === true && ctx.status === 200) {
+    let trackPageview = ctx.state.trackPageview && ctx.status === 200;
+    if (trackPageview && knownRobots[userAgent]) {
+      trackPageview = false;
+    }
+    if (trackPageview) {
       const rpath = ctx.request.path;
       const proto = ctx.get('x-forwarded-proto') || 'http';
       const host = ctx.get('host') || 'www.hebcal.com';
