b69d29133fa3fa594e1cbebe3b367c55df7135a9
Michael J. Radwin <michael@radwin.org>
2022-03-07 21:40:06 -0800

    Use URLSearchParams API

--- src/shortUrlRedir.js
+++ src/shortUrlRedir.js
@@ -2,48 +2,39 @@ import path from 'path';
 
 // eslint-disable-next-line require-jsdoc
 export function shortUrlRedir(ctx) {
-  const qs = Object.assign({}, ctx.request.query);
-  if (qs.uc) {
-    qs.utm_campaign = qs.uc;
-    if (qs.uc.startsWith('pdf-')) {
-      if (!qs.us && !qs.utm_source) {
-        qs.utm_source = 'pdf';
-      }
-      if (!qs.um && !qs.utm_medium) {
-        qs.utm_medium = 'document';
-      }
-    } else if (qs.uc.startsWith('ical-')) {
-      if (!qs.us && !qs.utm_source) {
-        qs.utm_source = 'js';
-      }
-      if (!qs.um && !qs.utm_medium) {
-        qs.utm_medium = 'icalendar';
-      }
+  const qs = new URLSearchParams(ctx.request.query);
+  let utmSource = qs.get('utm_source') || qs.get('us');
+  let utmMedium = qs.get('utm_medium') || qs.get('um');
+  if (qs.has('uc')) {
+    const campaign = qs.get('uc');
+    if (campaign.startsWith('pdf-')) {
+      utmSource = utmSource || 'pdf';
+      utmMedium = utmMedium || 'document';
+    } else if (campaign.startsWith('ical-')) {
+      utmSource = utmSource || 'js';
+      utmMedium = utmMedium || 'icalendar';
     }
-    delete qs.uc;
-  }
-  if (qs.us) {
-    qs.utm_source = qs.us;
-    delete qs.us;
-  }
-  if (qs.um) {
-    qs.utm_medium = qs.um;
-    delete qs.um;
+    qs.set('utm_campaign', campaign);
+    qs.delete('uc');
   }
   const ref = ctx.get('referer');
-  if (!qs.utm_source && ref && ref.length) {
+  if (!utmSource && ref && ref.length) {
     const refUrl = new URL(ref);
     if (!refUrl.hostname.endsWith('hebcal.com')) {
-      qs.utm_source = refUrl.hostname;
+      utmSource = refUrl.hostname;
     }
   }
-  qs.utm_source = qs.utm_source || 'redir';
-  qs.utm_medium = qs.utm_medium || 'redir';
-  qs._r = Date.now().toString(36);
+  utmSource = utmSource || 'redir';
+  utmMedium = utmMedium || 'redir';
+  qs.set('utm_source', utmSource);
+  qs.set('utm_medium', utmMedium);
+  qs.set('_r', Date.now().toString(36));
+  qs.delete('us');
+  qs.delete('um');
   const rpath = ctx.request.path;
   const base = path.basename(rpath);
   const dest = rpath.startsWith('/h/') ? 'holidays' : 'sedrot';
-  const destUrl = `https://www.hebcal.com/${dest}/${base}?` + new URLSearchParams(qs).toString();
+  const destUrl = `https://www.hebcal.com/${dest}/${base}?` + qs.toString();
   ctx.set('Cache-Control', 'private');
   ctx.redirect(destUrl);
 }
