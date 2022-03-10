8506c4f0bf91d4de66a692f7037cadc8b9400ecf
Michael J. Radwin <michael@radwin.org>
2022-03-10 11:16:12 -0800

    Remove random parameter from holidays redir as it's messing up AdSense

--- src/shortUrlRedir.js
+++ src/shortUrlRedir.js
@@ -28,12 +28,14 @@ export function shortUrlRedir(ctx) {
   utmMedium = utmMedium || 'redir';
   qs.set('utm_source', utmSource);
   qs.set('utm_medium', utmMedium);
-  qs.set('_r', Date.now().toString(36));
   qs.delete('us');
   qs.delete('um');
   const rpath = ctx.request.path;
   const base = path.basename(rpath);
   const dest = rpath.startsWith('/h/') ? 'holidays' : 'sedrot';
+  if (dest === 'sedrot') {
+    qs.set('_r', Date.now().toString(36));
+  }
   const destUrl = `https://www.hebcal.com/${dest}/${base}?` + qs.toString();
   ctx.set('Cache-Control', 'private');
   ctx.redirect(destUrl);
