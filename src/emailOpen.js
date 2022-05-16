8769cf9c363dc7296fa2c543c49681cfa6902145
Michael J. Radwin <michael@radwin.org>
2022-05-16 11:40:36 -0700

    Experiment with email open event tracking

--- src/emailOpen.js
+++ src/emailOpen.js
@@ -1,4 +1,5 @@
 import send from 'koa-send';
+import {matomoTrack} from './matomoTrack';
 
 const DOCUMENT_ROOT = '/var/www/html';
 
@@ -7,16 +8,11 @@ export async function emailOpen(ctx) {
   ctx.state.trackPageview = false;
   ctx.set('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
   ctx.type = 'image/gif';
-  const visitor = ctx.state.visitor;
-  if (visitor) {
-    const params = {
-      ec: 'email-open',
-      ea: ctx.request.query['utm_campaign'],
-      el: ctx.request.query['loc'],
-      p: ctx.request.path,
-      dh: ctx.get('host') || 'www.hebcal.com',
-    };
-    visitor.event(params).send();
-  }
+  matomoTrack(ctx, {
+    action_name: 'Email Open',
+    e_c: 'email-open',
+    e_a: ctx.request.query['utm_campaign'],
+    e_n: ctx.request.query['loc'],
+  });
   return send(ctx, '/__utm.gif', {root: DOCUMENT_ROOT});
 }
---
+++ src/matomoTrack.js
@@ -0,0 +1,40 @@
+import https from 'node:https';
+
+/**
+ * @param {any} ctx
+ * @param {any} params
+ */
+export function matomoTrack(ctx, params) {
+  const args = new URLSearchParams(params);
+  for (const p of ['idsite', 'rec', 'apiv']) {
+    args.set(p, '1');
+  }
+  const ipAddress = ctx.get('x-client-ip') || ctx.request.ip;
+  args.set('ua', ctx.get('user-agent'));
+  args.set('cip', ipAddress);
+  const ref = ctx.get('referer');
+  if (ref && ref.length) {
+    args.set('urlref', ref);
+  }
+  args.set('action_name', action);
+  if (ctx.state.userId) {
+    args.set('uid', ctx.state.userId);
+  }
+  const postData = args.toString();
+  const options = {
+    hostname: 'www.hebcal.com',
+    port: 443,
+    path: '/matomo/matomo.php',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(postData),
+    },
+  };
+  const req = https.request(options);
+  req.on('error', (err) => {
+    ctx.logger.error(err);
+  });
+  req.write(postData);
+  req.end();
+}
