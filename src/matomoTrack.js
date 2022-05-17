994021b34ae2326489baaa4d123f47fa15f6a4cf
Michael J. Radwin <michael@radwin.org>
2022-05-16 17:12:38 -0700

    Add Plausible tracking, too

--- src/matomoTrack.js
+++ src/matomoTrack.js
@@ -1,4 +1,5 @@
 import http from 'node:http';
+import {getIpAddress} from './common';
 
 /**
  * @param {any} ctx
@@ -9,8 +10,6 @@ export function matomoTrack(ctx, params) {
   for (const p of ['idsite', 'rec', 'apiv']) {
     args.set(p, '1');
   }
-  // const ipAddress = ctx.get('x-client-ip') || ctx.request.ip;
-  // args.set('cip', ipAddress);
   args.set('ua', ctx.get('user-agent'));
   args.set('lang', ctx.get('accept-language'));
   const ref = ctx.get('referer');
@@ -21,15 +20,17 @@ export function matomoTrack(ctx, params) {
     args.set('uid', ctx.state.userId);
   }
   const postData = args.toString();
+  const ip = getIpAddress(ctx);
   const options = {
     hostname: 'www-internal.hebcal.com',
     port: 8080,
     path: '/matomo/matomo.php',
     method: 'POST',
     headers: {
+      'X-Forwarded-For': ip,
+      'X-Forwarded-Proto': 'https',
       'Content-Type': 'application/x-www-form-urlencoded',
       'Content-Length': Buffer.byteLength(postData),
-      'X-Forwarded-Proto': 'https',
     },
   };
   const req = http.request(options);
@@ -38,4 +39,61 @@ export function matomoTrack(ctx, params) {
   });
   req.write(postData);
   req.end();
+  plausibleTrack(ctx, params);
+}
+
+/**
+ * @param {any} ctx
+ * @param {any} params
+ */
+export function plausibleTrack(ctx, params) {
+  const body = {
+    domain: 'hebcal.com',
+    name: 'event',
+    url: makeUrl(ctx),
+    props: params,
+  };
+  const ref = ctx.get('referer');
+  if (ref && ref.length) {
+    body.referrer = ref;
+  }
+  if (ctx.state.userId) {
+    body.props.uid = ctx.state.userId;
+  }
+  const postData = JSON.stringify(body);
+  const ip = getIpAddress(ctx);
+  const options = {
+    hostname: 'internal.analytics.hebcal.com',
+    port: 8000,
+    path: '/api/event',
+    method: 'POST',
+    headers: {
+      'User-Agent': ctx.get('user-agent'),
+      'X-Forwarded-For': ip,
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(postData),
+    },
+  };
+  const req = http.request(options);
+  req.on('error', (err) => {
+    ctx.logger.error(err);
+  });
+  req.write(postData);
+  req.end();
+}
+
+/**
+ * @param {*} ctx
+ * @return {string}
+ */
+function makeUrl(ctx) {
+  const rpath = ctx.request.path;
+  const proto = ctx.get('x-forwarded-proto') || 'http';
+  const host = ctx.get('host') || 'www.hebcal.com';
+  const qs = ctx.request.querystring;
+  let url = `${proto}://${host}${rpath}`;
+  if (qs && qs.length) {
+    url += `?${qs}`;
+  }
+  return url;
 }
