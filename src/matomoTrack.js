e22c3961d8356cddd6a482710b9b1c75e37cd5ca
Michael J. Radwin <michael@radwin.org>
2022-06-03 18:29:02 -0700

    Add another couple of known robots

--- src/matomoTrack.js
+++ src/matomoTrack.js
@@ -1,18 +1,20 @@
 import http from 'node:http';
 
 const knownRobots = {
+  'check_http/v2.2 (monitoring-plugins 2.2)': 1,
+  'GuzzleHttp/7': 1,
+  'kube-probe/1.21': 1,
   'Mediapartners-Google': 1,
   'Mozilla/5.0 (compatible; AhrefsBot/7.0; +http://ahrefs.com/robot/)': 1,
+  'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)': 1,
   'Mozilla/5.0 (compatible; BLEXBot/1.0; +http://webmeup-crawler.com/)': 1,
   'Mozilla/5.0 (compatible; DotBot/1.2; +https://opensiteexplorer.org/dotbot; help@moz.com)': 1,
   'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)': 1,
+  'Mozilla/5.0 (compatible; MJ12bot/v1.4.8; http://mj12bot.com/)': 1,
   'Mozilla/5.0 (compatible; SemrushBot/7~bl; +http://www.semrush.com/bot.html)': 1,
   'Mozilla/5.0 (compatible; YandexBot/3.0; +http://yandex.com/bots)': 1,
-  'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)': 1,
   'Rainmeter WebParser plugin': 1,
   'Varnish Health Probe': 1,
-  'check_http/v2.2 (monitoring-plugins 2.2)': 1,
-  'kube-probe/1.21': 1,
 };
 
 const isProduction = process.env.NODE_ENV === 'production';
