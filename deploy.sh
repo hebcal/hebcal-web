76417e9d03a47bb2ca0ddfdd5a5d2dd26fee58ed
Michael J. Radwin <michael@radwin.org>
2022-08-21 08:29:49 -0700

    Add new deploy script

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -x
+
+compress_file() {
+    file=$1
+    brotli -f --keep --best "${file}"
+    gzip -f --keep --best "${file}"
+}
+
+if [ ! -e node_modules/geolite2-redist/dbs/GeoLite2-City.mmdb ]; then
+  node -e "import('geolite2-redist').then(geolite => geolite.downloadDbs())"
+fi
+
+mkdir -p ./dist && cp ./src/*.json ./dist
+
+npx rollup -c
+
+compress_file ./static/i/$npm_package_config_sprite
+compress_file ./static/i/$npm_package_config_clientapp
+compress_file ./static/i/$npm_package_config_typeaheadcss
+compress_file ./static/i/$npm_package_config_csprite
+
+# for testing out of local
+rsync -av ./views/ ./dist/views/
+
+# deploy for prod
+rsync -av ./fonts/ /var/www/fonts/
+rsync -av ./static/ /var/www/html/
+rsync -av ./package.json /var/www/
+rsync -av ./dist/ /var/www/dist/
+rsync -av ./node_modules/ /var/www/node_modules/
+rsync -av ./node_modules/geolite2-redist/dbs/GeoLite2-*.mmdb /var/www/
