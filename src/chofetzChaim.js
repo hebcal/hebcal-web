406841cad0ac84cbe73293535cde12ecc633d04d
Michael J. Radwin <michael@radwin.org>
2023-04-14 04:51:53 -0700

    Get feed of Chafetz Chaim working

--- src/chofetzChaim.js
+++ src/chofetzChaim.js
@@ -1,4 +1,7 @@
-import {months, HDate, Event, flags, Locale, gematriya} from '@hebcal/core';
+import {months, HDate, Event, flags, Locale} from '@hebcal/core';
+import {eventsToIcalendar} from '@hebcal/icalendar';
+import {eventsToCsv} from '@hebcal/rest-api';
+import fs from 'fs';
 
 const Nisan = months.NISAN;
 const Iyyar = months.IYYAR;
@@ -411,10 +414,30 @@ class ChofetzChaimEvent extends Event {
   }
 }
 
-const hd = new HDate(15, 'Tishrei', 5781);
-const a = chofetzChaim(hd);
-console.log(a);
-const b = new ChofetzChaimEvent(hd, a);
-console.log(b.render('en'));
-console.log(b.url());
-console.log(b);
+(async function() {
+  const events = [];
+  const dt = new Date();
+  const year = dt.getFullYear();
+  const start = new HDate(new Date(year - 1, 11, 1));
+  const end = new HDate(new Date(year + 1, 11, 31));
+  const startAbs = start.abs();
+  const endAbs = end.abs();
+  for (let abs = startAbs; abs <= endAbs; abs++) {
+    const hd = new HDate(abs);
+    const reading = chofetzChaim(hd);
+    const ev = new ChofetzChaimEvent(hd, reading);
+    events.push(ev);
+  }
+  const icalStream = fs.createWriteStream('chofetz-chaim.ics');
+  const str = await eventsToIcalendar(events, {
+    title: 'Daily Chofetz Chaim',
+    caldesc: 'Daily study of the Sefer Chofetz Chaim, which deals with the Jewish ethics and laws of speech',
+    relcalid: 'a2a058a4-d2eb-45d8-9142-5976d4c85ebc',
+  });
+  icalStream.write(str);
+  icalStream.close();
+
+  const csvStream = fs.createWriteStream('chofetz-chaim.csv');
+  csvStream.write(eventsToCsv(events, {}));
+  csvStream.close();
+})();
