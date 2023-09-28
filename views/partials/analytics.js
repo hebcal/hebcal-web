804ee1d04e0c99a71a3a5f5dcbc027f00b0a6e29
Michael J. Radwin <michael@radwin.org>
2023-09-28 16:02:50 -0700

    Change client-side JS from var to const

--- views/partials/analytics.js
+++ views/partials/analytics.js
@@ -1,16 +1,16 @@
-/* eslint-disable no-var, one-var, keyword-spacing, comma-spacing, brace-style, space-before-blocks, require-jsdoc */
+/* eslint-disable keyword-spacing, comma-spacing, brace-style, space-before-blocks, require-jsdoc */
 var _paq = window._paq = window._paq || [];
-var canonicalMeta=document.querySelector('link[rel="canonical"]');
-var urlHref=canonicalMeta?canonicalMeta.href:window.location.href;
+const canonicalMeta=document.querySelector('link[rel="canonical"]');
+const urlHref=canonicalMeta?canonicalMeta.href:window.location.href;
 /* save utm */
-var utm_ = {};
+const utm_ = {};
 new URL(window.location.href).searchParams.forEach(function(v, k) {
   if (k[0]=='u'&&k[1]=='t'&&k[2]=='m'&&k[3]=='_') {utm_[k] = v;}
 });
 /* redact identifier from URL */
-var url = new URL(urlHref);
-var sp = url.searchParams;
-var pn = url.pathname;
+const url = new URL(urlHref);
+const sp = url.searchParams;
+const pn = url.pathname;
 if (pn.substring(0, 15)=='/yahrzeit/edit/') {
   url.pathname='/yahrzeit/edit/_ID_';
 } else if (pn.substring(0, 17)=='/yahrzeit/verify/') {
@@ -24,7 +24,7 @@ if (pn.substring(0, 15)=='/yahrzeit/edit/') {
     url.pathname='/converter/'+sp.get('gy')+'/'+sp.get('gm')+'/'+sp.get('gd');
   }
 } else if (pn=='/hebcal') {
-  var yr=sp.get('year');
+  const yr=sp.get('year');
   url.pathname=sp.get('v')=='1'&&yr?'/hebcal/'+yr:'/hebcal';
 }
 /* always remove search params */
@@ -37,25 +37,25 @@ _paq.push(['disableCookies']);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 /* Hebcal anonymous userid */
-var d0=document, c0=d0.cookie;
+const d0=document; const c0=d0.cookie;
 if(c0&&c0.length&&typeof URLSearchParams=='function'){
-  var cks=c0.split(';').map(function(s0){var s=s0.trim(),eq=s.indexOf('=');
-    return[s.substring(0, eq),s.substring(eq+1)]}).reduce(function(m,v){
-    m[v[0]]=v[1];return m},{});
+  const cks=c0.split(';').map(function(s0){const s=s0.trim(); const eq=s.indexOf('=');
+    return[s.substring(0, eq),s.substring(eq+1)];}).reduce(function(m,v){
+    m[v[0]]=v[1]; return m;},{});
   if(cks['C']){
-    var ck=new URLSearchParams(cks['C']);
-    var uid=ck.get('uid');
+    const ck=new URLSearchParams(cks['C']);
+    const uid=ck.get('uid');
     if(uid){
-      var vid = uid.substring(0, 4) + uid.substring(24);
+      const vid = uid.substring(0, 4) + uid.substring(24);
       _paq.push(['setVisitorId', vid]);
       _paq.push(['setUserId', uid]);
     }
   }
 }
 (function() {
-  var u='/ma/';
+  const u='/ma/';
   _paq.push(['setTrackerUrl', u+'ma.php']);
   _paq.push(['setSiteId', '1']);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  const d=document; const g=d.createElement('script'); const s=d.getElementsByTagName('script')[0];
   g.async=true; g.src=u+'ma.js'; s.parentNode.insertBefore(g,s);
 })();
