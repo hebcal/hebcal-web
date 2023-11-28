a7ca0e5f59da83d8ea510d377b4ddc12237bcd9e
Michael J. Radwin <michael@radwin.org>
2023-11-27 16:26:25 -0800

    geoUrlArgsDbl no longer used

--- src/shabbat-link.js
+++ src/shabbat-link.js
@@ -6,9 +6,8 @@ export async function shabbatJsLink(ctx) {
   const location0 = getLocationFromQuery(ctx.db, q);
   const location = location0 || ctx.db.lookupLegacyCity('New York');
   const geoUrlArgs = makeGeoUrlArgs2(q, location);
-  const geoUrlArgsDbl = geoUrlArgs.replace(/&/g, '&amp;');
   await ctx.render('link', {
-    q, geoUrlArgs, geoUrlArgsDbl,
+    q, geoUrlArgs,
     locationName: location.getName(),
     title: 'Embed Shabbat candle-lighting times in your website - Hebcal',
     langNames,
