e98a161bdfa835ffcc3c6999128d7ce900f89307
Michael J. Radwin <michael@radwin.org>
2023-12-09 21:52:12 -0800

    Change weekday CSV to use getLeyningOnDate

--- src/parsha-csv.js
+++ src/parsha-csv.js
@@ -1,11 +1,11 @@
 /* eslint-disable require-jsdoc */
 import {basename} from 'path';
 import {PassThrough} from 'stream';
-import {writeFullKriyahCsv, getLeyningForHoliday,
-  getLeyningForParsha, formatAliyahWithBook} from '@hebcal/leyning';
+import {writeFullKriyahCsv, getLeyningOnDate,
+  formatAliyahWithBook} from '@hebcal/leyning';
 import {writeTriennialCsv} from '@hebcal/triennial';
 import createError from 'http-errors';
-import {HebrewCalendar, HDate, months} from '@hebcal/core';
+import {HDate, months} from '@hebcal/core';
 
 const reFullKriyahIL = /^fullkriyah-il-(\d+).csv$/;
 const reFullKriyahDiaspora = /^fullkriyah-(\d+).csv$/;
@@ -67,68 +67,19 @@ function writeWeekdayCsv(stream, hyear, il) {
  */
 function writeWeekdayCsvInner(stream, hd, il) {
   // skip if it's not a Monday or Thursday, or if it's a holiday that has Torah reading
-  if (!isWeekdayReading(hd, il)) {
-    return false;
-  }
-  const hyear = hd.getFullYear();
-  const sedra = HebrewCalendar.getSedra(hyear, il);
-  const saturday = hd.onOrAfter(6);
-  const parsha = sedra.lookup(saturday);
-  if (!parsha.chag) {
-    writeWeekdayCsvLines(stream, parsha.parsha, hd);
-    return true;
-  }
-  // So it's a Monday or Thursday but Saturday is Chag.
-  // Search for next regular parsha, which could even spill into next year
-  const endOfYear = new HDate(1, months.TISHREI, hyear + 1).abs() - 1;
-  const endAbs = endOfYear + 30;
-  for (let sat2 = saturday.abs() + 7; sat2 <= endAbs; sat2 += 7) {
-    const sedra2 = sat2 > endOfYear ? HebrewCalendar.getSedra(hyear + 1, il) : sedra;
-    const parsha2 = sedra2.lookup(sat2);
-    if (!parsha2.chag) {
-      writeWeekdayCsvLines(stream, parsha2.parsha, hd);
-      return true;
-    }
-  }
-  throw createError(500, `Can't find weekday for ${hd.toString()}`);
-}
-
-/**
- * @private
- * @param {WriteStream} stream
- * @param {string[]} parsha
- * @param {HDate} hd
- */
-function writeWeekdayCsvLines(stream, parsha, hd) {
-  const reading = getLeyningForParsha(parsha);
-  const parshaName = parsha.join('-');
-  const date = fmtDate(hd.greg());
-  if (reading.weekday) {
-    writeCsvLines(stream, date, parshaName, reading.weekday);
-  } else {
-    throw createError(500, `No weekday for ${parshaName} on ${date}`);
-  }
-}
-
-/**
- * @private
- * @param {HDate} hd
- * @param {boolean} il
- * @return {boolean}
- */
-function isWeekdayReading(hd, il) {
   const dow = hd.getDay();
   if (dow !== 1 && dow !== 4) {
     return false;
   }
-  const events = HebrewCalendar.getHolidaysOnDate(hd, il) || [];
-  for (const ev of events) {
-    const reading = getLeyningForHoliday(ev, il);
-    if (reading && reading.fullkriyah) {
-      return false;
+  const readings = getLeyningOnDate(hd, il, true);
+  for (const reading of readings) {
+    if (reading?.weekday) {
+      const date = fmtDate(hd.greg());
+      writeCsvLines(stream, date, reading.name.en, reading.weekday);
+      return true;
     }
   }
-  return true;
+  return false;
 }
 
 const fmt = new Intl.DateTimeFormat('en-US', {
