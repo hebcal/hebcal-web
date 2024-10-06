ae040cfb941b50efe8f8c0ff39a43a37fd094af0
Michael J. Radwin <michael@radwin.org>
2024-10-06 12:42:58 -0700

    Even shorter URL redirect for parshiyot

--- src/shortUrlRedir.js
+++ src/shortUrlRedir.js
@@ -1,3 +1,8 @@
+import {basename, dirname} from 'node:path';
+import {HebrewCalendar, parshiot} from '@hebcal/core';
+import {makeAnchor} from '@hebcal/rest-api';
+import dayjs from 'dayjs';
+
 const shortToLong = {
   h: 'holidays',
   s: 'sedrot',
@@ -41,7 +46,62 @@ export function shortUrlRedir(ctx) {
   if (!dest) {
     ctx.throw(500, `Unknown short URL '${shortStr}'`);
   }
-  const destUrl = `https://www.hebcal.com/${dest}/${base}?` + qs.toString();
+  let destUrl;
+  if (dest === 'sedrot') {
+    destUrl = shortParshaRedir(ctx, base, qs);
+  }
+  if (!destUrl) {
+    destUrl = `https://www.hebcal.com/${dest}/${base}?` + qs.toString();
+  }
   ctx.set('Cache-Control', 'private');
   ctx.redirect(destUrl);
+}
+
+function isValidDouble(id) {
+  switch (id) {
+    case 21: // Vayakhel-Pekudei
+    case 26: // Tazria-Metzora
+    case 28: // Achrei Mot-Kedoshim
+    case 31: // Behar-Bechukotai
+    case 38: // Chukat-Balak
+    case 41: // Matot-Masei
+    case 50: // Nitzavim-Vayeilech
+      return true;
+  }
+  return false;
+}
+
+function shortParshaRedir(ctx, str, qs) {
+  const code = str.charCodeAt(0);
+  if (code < 48 || code > 57) {
+    return false; // not a number, let old redirect logic happen
+  }
+  const parshaStr = basename(str);
+  const parshaId = parseInt(parshaStr, 10) - 1;
+  if (isNaN(parshaId) || parshaId > parshiot.length - 1) {
+    ctx.throw(400, `invalid short redirect parsha: ${parshaStr}`);
+  }
+  const doubled = parshaStr.endsWith('d');
+  if (doubled && !isValidDouble(parshaId)) {
+    ctx.throw(400, `invalid short redirect double parsha: ${parshaStr}`);
+  }
+  const yearStr = dirname(str);
+  const year = parseInt(yearStr, 10);
+  const il = yearStr.endsWith('i');
+  const sedra = HebrewCalendar.getSedra(year, il);
+  const hd = sedra.find(doubled ? -parshaId : parshaId);
+  if (hd === null) {
+    ctx.throw(404, `short redirect not found: ${str}`);
+    return false;
+  }
+  const name0 = doubled ?
+      (parshiot[parshaId] + '-' + parshiot[parshaId + 1]) :
+      parshiot[parshaId];
+  const name = makeAnchor(name0);
+  const d = dayjs(hd.greg());
+  const fmtDt = d.format('YYYYMMDD');
+  if (il) {
+    qs.set('i', 'on');
+  }
+  return `https://www.hebcal.com/sedrot/${name}-${fmtDt}?` + qs.toString();
 }
