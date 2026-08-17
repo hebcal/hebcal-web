bc56d06a27088931ce9676294307760e76dcf0f9
Michael J. Radwin <michael@radwin.org>
2026-08-17 08:39:42 -0700

    Remove /holidays/hebcal-*.pdf rendering from app-www; it is now served exclusively by hebcal-api-go

--- src/holidayApp.js
+++ src/holidayApp.js
@@ -1,6 +1,5 @@
 import {holidayDetail} from './holidayDetail.js';
 import {holidayMainIndex} from './holidayIndex.js';
-import {holidayPdf} from './holidayPdf.js';
 import {holidayYearIndex} from './holidayYearIndex.js';
 import {isDigit} from './isDigit.js';
 
@@ -10,7 +9,9 @@ export async function holidayApp(ctx) {
   if (rpath === '/holidays/') {
     await holidayMainIndex(ctx);
   } else if (rpath.endsWith('.pdf')) {
-    await holidayPdf(ctx);
+    // Remove /holidays/hebcal-*.pdf rendering from app-www.
+    // PDF rendering now served exclusively by https://github.com/hebcal/hebcal-api-go
+    ctx.throw(501, 'This service does not render PDFs');
   } else if (isDigit(rpath, 10)) {
     await holidayYearIndex(ctx);
   } else {
