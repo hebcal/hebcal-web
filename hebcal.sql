bd41906f2ee8b4f4cf8299f005425ff9780c4444
Michael J. Radwin <michael@radwin.org>
2026-09-23 14:18:39 -0700

    drop email_open.delta column

--- hebcal.sql
+++ hebcal.sql
@@ -97,7 +97,6 @@ CREATE TABLE email_open (
   msgid varchar(80) NOT NULL,
   ip_addr varchar(39) DEFAULT NULL,
   loc varchar(80) DEFAULT NULL,
-  delta int DEFAULT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=ascii;
 
